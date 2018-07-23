70626a80291fe1060ab9d2348234a92af62a1d86
Yufeng <xiang.yufeng@outlook.com>
2018-07-22 23:33:05 -0400

    Minor Update

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
 const Posts = require('../models/posts');
 
 const indexRouter = express.Router();
+const MarkdownIt = require('markdown-it');
+var md = new MarkdownIt();
 
 
 indexRouter.route('/')
 .get((req,res,next) => {
     Posts.find({})
     .then((posts) => {
+        posts.forEach(post => {
+            if (!post.abstract) {
+                post.content = md.render(post.content);
+                post.abstract = post.content.replace(/(<([^>]+)>)/ig,"").substring(0,150) + '...';
+            }
+        });
         res.render('index', {posts: posts});
     }, (err) => next(err))
     .catch((err) => next(err));
@@ -18,5 +23,4 @@ indexRouter.route('/')
 .post((req,res,next) => {
     res.statusCode = 403;
 });
-
 module.exports = indexRouter;
